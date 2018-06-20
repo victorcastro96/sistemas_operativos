8ecdb87245d9a86076c255a9daec27da94cd84dd
victorcastro96 <36208131+victorcastro96@users.noreply.github.com>
2018-06-20 11:45:54 -0500

    Add files via upload

---
+++ Documents/SOTR/Tutorial/paging.h
@@ -0,0 +1,132 @@
+#ifndef PAGING_H
+#define PAGING_H
+#include "commo.h"
+#include "isr.h"
+typedef struct page
+{
+   u32int present    : 1;   // Page present in memory
+   u32int rw         : 1;   // Read-only if clear, readwrite if set
+   u32int user       : 1;   // Supervisor level only if clear
+   u32int accessed   : 1;   // Has the page been accessed since last refresh?
+   u32int dirty      : 1;   // Has the page been written to since last refresh?
+   u32int unused     : 7;   // Amalgamation of unused and reserved bits
+   u32int frame      : 20;  // Frame address (shifted right 12 bits)
+} page_t;
+
+typedef struct page_table
+{
+   page_t pages[1024];
+} page_table_t;
+
+typedef struct page_directory
+{
+   /**
+      Array of pointers to pagetables.
+   **/
+   page_table_t *tables[1024];
+   /**
+      Array of pointers to the pagetables above, but gives their *physical*
+      location, for loading into the CR3 register.
+   **/
+   u32int tablesPhysical[1024];
+   /**
+      The physical address of tablesPhysical. This comes into play
+      when we get our kernel heap allocated and the directory
+      may be in a different location in virtual memory.
+   **/
+   u32int physicalAddr;
+} page_directory_t;
+
+/**
+  Sets up the environment, page directories etc and
+  enables paging.
+**/
+void initialise_paging();
+
+/**
+  Causes the specified page directory to be loaded into the
+  CR3 register.
+**/
+void switch_page_directory(page_directory_t *new);
+
+/**
+  Retrieves a pointer to the page required.
+  If make == 1, if the page-table in which this page should
+  reside isn't created, create it!
+**/
+page_t *get_page(u32int address, int make, page_directory_t *dir);
+
+/**
+  Handler for page faults.
+**/
+void page_fault(registers_t regs);
+
+void initialise_paging()
+{
+   // The size of physical memory. For the moment we
+   // assume it is 16MB big.
+   u32int mem_end_page = 0x1000000;
+
+   nframes = mem_end_page / 0x1000;
+   frames = (u32int*)kmalloc(INDEX_FROM_BIT(nframes));
+   memset(frames, 0, INDEX_FROM_BIT(nframes));
+
+   // Let's make a page directory.
+   kernel_directory = (page_directory_t*)kmalloc_a(sizeof(page_directory_t));
+   memset(kernel_directory, 0, sizeof(page_directory_t));
+   current_directory = kernel_directory;
+
+   // We need to identity map (phys addr = virt addr) from
+   // 0x0 to the end of used memory, so we can access this
+   // transparently, as if paging wasn't enabled.
+   // NOTE that we use a while loop here deliberately.
+   // inside the loop body we actually change placement_address
+   // by calling kmalloc(). A while loop causes this to be
+   // computed on-the-fly rather than once at the start.
+   int i = 0;
+   while (i < placement_address)
+   {
+       // Kernel code is readable but not writeable from userspace.
+       alloc_frame( get_page(i, 1, kernel_directory), 0, 0);
+       i += 0x1000;
+   }
+   // Before we enable paging, we must register our page fault handler.
+   register_interrupt_handler(14, page_fault);
+
+   // Now, enable paging!
+   switch_page_directory(kernel_directory);
+}
+
+void switch_page_directory(page_directory_t *dir)
+{
+   current_directory = dir;
+   asm volatile("mov %0, %%cr3":: "r"(&dir->tablesPhysical));
+   u32int cr0;
+   asm volatile("mov %%cr0, %0": "=r"(cr0));
+   cr0 |= 0x80000000; // Enable paging!
+   asm volatile("mov %0, %%cr0":: "r"(cr0));
+}
+
+page_t *get_page(u32int address, int make, page_directory_t *dir)
+{
+   // Turn the address into an index.
+   address /= 0x1000;
+   // Find the page table containing this address.
+   u32int table_idx = address / 1024;
+   if (dir->tables[table_idx]) // If this table is already assigned
+   {
+       return &dir->tables[table_idx]->pages[address%1024];
+   }
+   else if(make)
+   {
+       u32int tmp;
+       dir->tables[table_idx] = (page_table_t*)kmalloc_ap(sizeof(page_table_t), &tmp);
+       memset(dir->tables[table_idx], 0, 0x1000);
+       dir->tablesPhysical[table_idx] = tmp | 0x7; // PRESENT, RW, US.
+       return &dir->tables[table_idx]->pages[address%1024];
+   }
+   else
+   {
+       return 0;
+   }
+}
